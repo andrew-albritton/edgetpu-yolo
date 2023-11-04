11134cc7dd8332812b46226527a845c1244c589e
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-04 22:37:05 +0000

    anotha thing

--- edgetpumodel.py
+++ edgetpumodel.py
@@ -265,8 +265,6 @@ class EdgeTPUModel:
                 data = {'Prediction': output[base]['cls_name'], 'Confidence': output[base]['conf']}
                 with open(csv_path, mode='a', newline='') as f:
                     writer = csv.DictWriter(f, fieldnames=data.keys())
-                    if not csv_path.is_file():
-                        writer.writeheader()
                     writer.writerow(data)
             if save_img:
               cv2.imwrite(output_path, output_image)
