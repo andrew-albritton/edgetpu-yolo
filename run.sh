28cc4a53b901a0f8229eeee37ae39a012726e179
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-07 00:53:28 +0000

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 detect.py -m best-int8_edgetpu.tflite --names data.yaml --stream True --device /dev/video1:YUYV:800x600:20/1
+python3 detect.py -m best-int8_edgetpu.tflite --names data.yaml --stream True --device 1
