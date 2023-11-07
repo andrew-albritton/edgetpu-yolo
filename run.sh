ebed41c60357e2eb0248749d81df2aab5144c3fa
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-06 18:43:29 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 detect.py -m best-int8_edgetpu.tflite --names data.yaml --image processed_images/featured-image-drain-fly.webp
+python3 detect.py -m best-int8_edgetpu.tflite --names data.yaml --stream True --device /dev/video1:YUYV:800x600:20/1
