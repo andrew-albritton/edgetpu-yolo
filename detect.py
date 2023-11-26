a25b563388969c8c4e40b9ad4032f1aa6f99f6af
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-26 14:35:08 -0600

    Update detect.py

--- detect.py
+++ detect.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
             else:
                 full_image, net_image, pad = get_image_tensor(image, input_size[0])
                 pred = model.forward(net_image)
-                imagepath = "out_images_/"+photos+".jpg"
+                imagepath = photos+".jpg"
                 model.process_predictions(pred[0], full_image, pad, output_path=imagepath)
                 ImageFile.LOAD_TRUNCATED_IMAGES = True
                 image_files = []
