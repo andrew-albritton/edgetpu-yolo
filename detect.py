96119232c58c57b369e2819d41993b2df8bdc49d
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-26 15:50:30 -0600

    Update detect.py

--- detect.py
+++ detect.py
@@ -153,19 +153,19 @@ if __name__ == "__main__":
                     
         
         fps = 5
-        
-        clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip("out_images_/", fps=fps)
-        clip.write_videofile('my_new_video.mp4')
-        folder = 'out_images_/'
-        for filename in os.listdir(folder):
-          file_path = os.path.join(folder, filename)
-          try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-              os.unlink(file_path)
-            elif os.path.isdir(file_path):
-              shutil.rmtree(file_path)
-          except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        if os.stat("out_images_").st_size > 0:
+          clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip("out_images_/", fps=fps)
+          clip.write_videofile('my_new_video.mp4')
+          folder = 'out_images_/'
+          for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+              if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+              elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            except Exception as e: 
+              print('Failed to delete %s. Reason: %s' % (file_path, e))
         cam.release()
             
         
