13853823e1011be4db59eeb28fe9508da3a42521
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-27 11:03:54 -0600

    Update I2C_board_to_power_module.py

--- I2C_board_to_power_module.py
+++ I2C_board_to_power_module.py
@@ -6,7 +6,7 @@ try:
     while True:
         try:
             i2c = I2C("/dev/i2c-1")
-            data = 0x00
+            data = [0x00]
             msg = I2C.Message(data, read=True)
 
             print(data)
