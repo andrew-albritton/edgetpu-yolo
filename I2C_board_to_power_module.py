d720019de290e320503aa9ef8271940b3c7dc80c
drasafasforis <90645699+drasafasforis@users.noreply.github.com>
2023-11-27 11:03:17 -0600

    Update I2C_board_to_power_module.py

--- I2C_board_to_power_module.py
+++ I2C_board_to_power_module.py
@@ -6,10 +6,10 @@ try:
     while True:
         try:
             i2c = I2C("/dev/i2c-1")
+            data = 0x00
+            msg = I2C.Message(data, read=True)
 
-            msg = I2C.Message([0x00], read=True)
-
-            print(msg)
+            print(data)
             
         except Exception as e:
             print(f"An error occurered: {e}")
