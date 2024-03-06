aaee002eb51de74b80f5a87345c868ad978b4b28
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-06 15:29:42 -0600

    Update SPI_test.py
    This will test coral board spi

--- SPI_test.py
+++ SPI_test.py
@@ -1,1 +1,15 @@
+from periphery import SPI
 
+spi1_0 = SPI("/dev/spidev0.0", 0, 125000000,'lsb',1)
+
+data = '{0:040b}'.format(40000)
+
+data_out = [int(x) for x in str(data)]
+data_out.reverse()
+
+datain = spi.transfer(data_out)
+
+print(data_out)
+print(datain)
+
+spi.close()
