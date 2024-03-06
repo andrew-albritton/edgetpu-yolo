238bc28b9121061e9cf77053c959c92eb4453a9b
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-06 14:39:42 -0600

    Update GPIO_boot_checker.py

--- GPIO_boot_checker.py
+++ GPIO_boot_checker.py
@@ -1,1 +1,16 @@
+from periphery import GPIO
+import time
+
+test_pin = GPIO("dev/gpiochip4", 13, "out")
+
+while(i < 10):
+  try:
+    test_pin.write(True)
+    time.sleep(5)
+    test_pin.write(False)
+    time.sleep(5)
+    i = i + 1
+  except Exception as e:
+    print(f"An error occured: {e}")
+    
 
