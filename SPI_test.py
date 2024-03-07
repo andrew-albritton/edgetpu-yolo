c88882d0e60d47d35360d4f2b9bc85399bd3460c
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 15:25:13 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -1,15 +1,19 @@
 from periphery import SPI
 
-while(1):
-  spi1_0 = SPI("/dev/spidev0.0", 0, 125000000)
-  
-  data = '{0:040b}'.format(40000)
-  
-  data_out = [int(x) for x in str(data)]
-  data_out.reverse()
-  
-  datain = spi1_0.transfer(data_out)
-  
+freq = frequency * 4294967295 / 125000000
   
 
-spi1_0.close()
+spi1_0 = SPI("/dev/spidev0.0", 0, 125000000)
+
+data = '{0:040b}'.format(freq)
+
+data_out = [int(x) for x in str(data)]
+data_out.reverse()
+try:
+  while(True):
+    try:
+      datain = spi1_0.transfer(data_out)
+    except Exception as e:
+      print(f"An error occurred: {e}")
+except KeyboardInterrupt:
+  spi1_0.close()
