88b33bbd0ca112c7163de25067f0d8891c712542
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 14:58:38 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -1,15 +1,17 @@
 from periphery import SPI
 
-spi1_0 = SPI("/dev/spidev0.0", 0, 125000000)
-
-data = '{0:040b}'.format(40000)
-
-data_out = [int(x) for x in str(data)]
-data_out.reverse()
-
-datain = spi1_0.transfer(data_out)
-
-print(data_out)
-print(datain)
+while(1):
+  spi1_0 = SPI("/dev/spidev0.0", 0, 125000000)
+  
+  data = '{0:040b}'.format(40000)
+  
+  data_out = [int(x) for x in str(data)]
+  data_out.reverse()
+  
+  datain = spi1_0.transfer(data_out)
+  
+  print(data_out)
+  print(datain)
+  
 
 spi1_0.close()
