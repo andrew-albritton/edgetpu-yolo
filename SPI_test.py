f91386c5f6c53a539ac69e6b32273a39b9b7d591
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 16:56:14 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -30,8 +30,8 @@ def sendFrequency(frequency):     		# Function to send frequency (assumes 125MHz
     tfr_byte(freq & 0xFF)
     print(freq & 0xFF)
     freq=freq>>8
-    #tfr_byte(0x00)
     pulseHigh(FQ_UD)
+  tfr_byte(0x00)
   return
 
 
