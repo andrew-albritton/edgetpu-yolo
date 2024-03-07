56eef28da45f7be3cc2e230e4e80fcc1b2fafa89
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 16:32:42 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -38,7 +38,7 @@ def sendFrequency(frequency):     		# Function to send frequency (assumes 125MHz
 
 
 
-frequency = [40000, 50000, 70000, 200000, 400000]               		# choose frequency and
+frequency = [4000, 5000, 7000, 20000, 40000]               		# choose frequency and
 for i in range(0, len(frequency)):
   pulseHigh(RESET)                  		# start-up sequence...
   pulseHigh(W_CLK)
