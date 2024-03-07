50830de2aee84e65552a9f6baa8ae7a24caae12c
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 16:07:43 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -38,5 +38,5 @@ pulseHigh(RESET)                  		# start-up sequence...
 pulseHigh(W_CLK)
 pulseHigh(FQ_UD)
 
-frequency = 470000               		# choose frequency and
+frequency = 475000               		# choose frequency and
 sendFrequency(frequency)          		# start the oscillator
