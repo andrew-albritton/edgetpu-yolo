a622d74d06cbbee5c4fb12494f3e1d719758c4b9
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-07 15:46:44 -0600

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -9,27 +9,27 @@ DATA = GPIO("/dev/gpiochip0", 7, "out")         #pin 29
 RESET = GPIO("/dev/gpiochip0", 8, "out")        #pin 31
 
 def pulseHigh(pin):               		# Function to send a pulse
-	pin.write(True)
+  pin.write(True)
   pin.write(True)
   pin.write(True)
   pin.write(False)# do it a few times to increase pulse width# (easier than writing a delay loop!)   	# end of the pulse
-	return
+  return
 
 def tfr_byte(data):               		# Function to send a byte by serial "bit-banging"
-	for i in range (0,8):
-		DATA.write(data & 0x01)	# Mask out LSB and put on GPIO pin "DATA"
-		pulseHigh(W_CLK)              	# pulse the clock line
-		data=data>>1                  	# Rotate right to get next bit
-	return
+  for i in range (0,8):
+    DATA.write(data & 0x01)	# Mask out LSB and put on GPIO pin "DATA"
+    pulseHigh(W_CLK)              	# pulse the clock line
+    data=data>>1                  	# Rotate right to get next bit
+  return
 
 def sendFrequency(frequency):     		# Function to send frequency (assumes 125MHz xtal)
-	freq=long(frequency*4294967296/125000000)
-	for b in range (0,4):
-		tfr_byte(freq & 0xFF)
-		freq=freq>>8
-	tfr_byte(0x00)
-	pulseHigh(FQ_UD)
-	return
+  freq=long(frequency*4294967296/125000000)
+  for b in range (0,4):
+    tfr_byte(freq & 0xFF)
+    freq=freq>>8
+    tfr_byte(0x00)
+    pulseHigh(FQ_UD)
+  return
 
 
 
