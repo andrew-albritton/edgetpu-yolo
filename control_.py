54884e058c99039da19d6ae72be5105a9b8ee635
andrew-albritton <145401563+andrew-albritton@users.noreply.github.com>
2024-03-21 15:52:42 -0500

    Update control_.py

--- control_.py
+++ control_.py
@@ -24,7 +24,39 @@ images for detection
 final descision label for transmission 
 '''
 
+W_CLK = GPIO("/dev/gpiochip4",10,"out")         #pin 18
+FQ_UD = GPIO("/dev/gpiochip4",12,"out")         #pin 22
+DATA = GPIO("/dev/gpiochip0", 7, "out")         #pin 29
+RESET = GPIO("/dev/gpiochip0", 8, "out")        #pin 31
 
+def pulseHigh(pin):               		# Function to send a pulse
+  pin.write(True)
+  pin.write(True)
+  pin.write(True)
+  pin.write(False)# do it a few times to increase pulse width# (easier than writing a delay loop!)   	# end of the pulse
+  return
+
+def tfr_byte(data):               		# Function to send a byte by serial "bit-banging"
+  for i in range (0,8):
+    if((data & 0x01) == 0x01):	# Mask out LSB and put on GPIO pin "DATA"
+      DATA.write(True)
+    elif((data & 0x01) == 0x00):
+      DATA.write(False)
+    pulseHigh(W_CLK)              	# pulse the clock line
+    data=data>>1                  	# Rotate right to get next bit
+  return
+
+def sendFrequency(frequency):     		# Function to send frequency (assumes 125MHz xtal)
+  freq=round((frequency*4294967295)/125000000)
+  print(freq)
+  for b in range (0,4):
+    tfr_byte(freq & 0xFF)
+    print(freq & 0xFF)
+    freq=freq>>8
+    
+  tfr_byte(0x00)
+  pulseHigh(FQ_UD)
+  return
 
 
 # Getting file_names_array using this function
@@ -127,6 +159,30 @@ try:
                     print("lizard averaged prob: ", lizardscore, '\n')
                     print("fly averaged    prob: ", flyscore, '\n')
                     print("Transmit Frequency: "+tot_count)
+                    if (tot_count == rat):
+                        frequency = 70000               		# choose frequency and
+                          pulseHigh(RESET)                  		# start-up sequence...
+                          pulseHigh(W_CLK)
+                          pulseHigh(FQ_UD)
+                          print("sending frequency: "+str(frequency))
+                          sendFrequency(frequency)          		# start the oscillator
+                          time.sleep(10)
+                    if (tot_count == lizard):
+                         frequency = 50000               		# choose frequency and
+                          pulseHigh(RESET)                  		# start-up sequence...
+                          pulseHigh(W_CLK)
+                          pulseHigh(FQ_UD)
+                          print("sending frequency: "+str(frequency))
+                          sendFrequency(frequency)          		# start the oscillator
+                          time.sleep(10)
+                    if (tot_count == fly):
+                         frequency = 40000               		# choose frequency and
+                          pulseHigh(RESET)                  		# start-up sequence...
+                          pulseHigh(W_CLK)
+                          pulseHigh(FQ_UD)
+                          print("sending frequency: "+str(frequency))
+                          sendFrequency(frequency)          		# start the oscillator
+                          time.sleep(10)
                     f = open("detections.csv", "w+")
                     f.close()
                     
